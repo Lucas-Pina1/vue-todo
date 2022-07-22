e0057256f54939c7362fa795892446ee5d279219
Lucas Pina <ti.lucaspina@gmail.com>
2022-07-21 21:03:37 -0300

    todo pronto sem router e vuex

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,81 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <div class="container grid-xs py-2">
+      <img
+        class="img-responsive img-logo"
+        src="./assets/logo.png"
+        alt="logo marca"
+      />
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="todo.description"
+            class="form-input"
+            placeholder="Novo todo"
+          />
+          <button class="btn btn-primary input-group btn">Adicionar</button>
+        </div>
+        <div class="todo-list">
+          <new-todo
+            v-for="task in todos"
+            :key="task.id"
+            @toggle="toggleTodo"
+            @remove="removeTodo"
+            :todo="task"
+          ></new-todo>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import newTodo from "./components/newTodo.vue";
+
+export default {
+  name: "app",
+  components: {
+    newTodo,
+  },
+  data() {
+    return {
+      todos: [],
+      todo: {
+        checked: false,
+      },
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked: false };
+    },
+    toggleTodo(todo) {
+      const index = this.todos.findIndex((item) => item.id === todo.id);
+      if (index > -1) {
+        const checked = !this.todos[index].checked;
+        this.$set(this.todos, index, { ...this.todos[index], checked });
+      }
+    },
+    removeTodo(todo) {
+      const index = this.todos.findIndex((item) => item.id === todo.id);
+      if (index > -1) {
+        this.$delete(this.todos, index);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.todo-list {
+  padding-top: 2rem;
 }
 </style>
